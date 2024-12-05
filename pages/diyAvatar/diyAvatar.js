6632c8d2728db82d5493db08486f073c7de4c907
czx0v0 <yatesara@hotmail.com>
2024-12-06 00:15:33 +0800

    更新自定义头像框界面

--- pages/diyAvatar/diyAvatar.js
+++ pages/diyAvatar/diyAvatar.js
@@ -7,7 +7,30 @@ Page({
    */
   data: {
     avatarUrl: defaultAvatarUrl,
-    canvas:{}
+    canvas: {},
+    diyType:'outer',
+    outerFrameUrl:'',
+    innerFrameUrl:'',
+    cornerFrameUrl:['','','',''],
+    outerFrames:[
+      {frameId:'0',frameSrc:'../../sources/img/none.png'},
+      {frameId:'1',frameSrc:'../../sources/img/avatar_frame_out_1.png'},
+      {frameId:'2',frameSrc:'../../sources/img/avatar_frame_out_2.png'},
+      {frameId:'3',frameSrc:'../../sources/img/avatar_frame_out_3.png'}
+    ],
+    innerFrames:[
+      {frameId:'10',frameSrc:'../../sources/img/none.png'},
+      {frameId:'11',frameSrc:'../../sources/img/avatar_frame_out_2.png'},
+      {frameId:'12',frameSrc:'../../sources/img/avatar_frame_out_3.png'}
+    ],
+    cornerFrames:[
+      // 用十位数字表示方位：0左上，1左下，2右上，3右下
+      {frameId:'100',frameSrc:'../../sources/img/none.png'},
+      {frameId:'101',frameSrc:'../../sources/img/ava1.png'},
+      {frameId:'112',frameSrc:'../../sources/img/ava2.png'},
+      {frameId:'123',frameSrc:'../../sources/img/ava3.png'},
+      {frameId:'134',frameSrc:'../../sources/img/ava4.png'},
+    ],
   },
 
   /**
@@ -15,7 +38,7 @@ Page({
    */
   onLoad(options) {
     this.loadAvatar(); //检查本地头像加载
-    this.drawAvatarFrame();//绘制头像和头像框
+    this.drawAvatarFrame(); //绘制头像和头像框
   },
   drawAvatarFrame() {
     // 存储this
@@ -30,7 +53,7 @@ Page({
         const canvas = res[0].node; // 获取canvas节点
         console.log(res)
         const ctx = canvas.getContext('2d');
-        
+
         // 获取2d上下文
         const dpr = wx.getSystemInfoSync().pixelRatio; //获取设备的像素比
         // 设置canvas宽高（实际）
@@ -64,115 +87,167 @@ Page({
               // 绘制头像框
               that.drawFrame(canvas, avatarWidth);
               console.log('canvas complete');
-              
+
             }
           }
         });
         that.data.canvas = canvas;
       })
-      
+
   },
   drawFrame(canvas, avatarWidth) {
-    const frameSize = avatarWidth ;
-    const ctx = canvas.getContext('2d');
-    // 头像框的位置
-    const framePosition = [{
-        x: 0,
-        y: frameSize * 0
-      },
-      // {
-      //   x: avatarWidth - frameSize,
-      //   y: 0
-      // },
-      // {
-      //   x: 0,
-      //   y: avatarWidth - frameSize
-      // },
-      // {
-      //   x: avatarWidth - frameSize * 1.3,
-      //   y: avatarWidth - frameSize * 1.3
-      // }
-    ];
-    framePosition.forEach((pos, index) => {
-      console.log(index);
-      const frameImg = canvas.createImage()
-      if (index == 0) {
-        frameImg.src = "../../sources/img/avatar_frame_out_2.png";
-      } else if (index == 1) {
-        frameImg.src = "../../sources/img/ava2.png";
-      } else if (index == 2) {
-        frameImg.src = "../../sources/img/ava3.png";
-      } else {
-        frameImg.src = "../../sources/img/ava4.png";
-      }
-      frameImg.onload = () => {
-        ctx.drawImage(frameImg, pos.x, pos.y, frameSize, frameSize);
-      }
-    });
+    var that = this;
+    // 绘制内框
+    if (this.data.innerFrameUrl != '') {
+      const frameSize = avatarWidth*0.8;
+      const ctx = canvas.getContext('2d');
+      // 头像框的位置
+      const framePosition = [{
+          x: avatarWidth*0.1,
+          y: avatarWidth*0.1
+        },
+      ];
+      framePosition.forEach((pos, index) => {
+        console.log(index);
+        const frameImg = canvas.createImage()
+        if (index == 0) {
+          frameImg.src = this.data.innerFrameUrl;
+        }
+        frameImg.onload = () => {
+          ctx.drawImage(frameImg, pos.x, pos.y, frameSize, frameSize);
+        }
+      });
+    }
+    // 绘制外框
+    if (this.data.outerFrameUrl != '') {
+      const frameSize = avatarWidth;
+      const ctx = canvas.getContext('2d');
+      // 头像框的位置
+      const framePosition = [{
+          x: 0,
+          y: 0
+        }
+      ];
+      framePosition.forEach((pos, index) => {
+        console.log(index);
+        const frameImg = canvas.createImage()
+        if (index == 0) {
+          frameImg.src = this.data.outerFrameUrl;
+        } 
+        frameImg.onload = () => {
+          ctx.drawImage(frameImg, pos.x, pos.y, frameSize, frameSize);
+        }
+      });
+    }
+    // 绘制四角边框
+    if (this.data.cornerFrameUrl[0]!=''||this.data.cornerFrameUrl[1]!=''||this.data.cornerFrameUrl[2]!=''||this.data.cornerFrameUrl[3]!='') {
+      const frameSize = avatarWidth*0.3;
+      const ctx = canvas.getContext('2d');
+      // 头像框的位置
+      const framePosition = [{
+          x: 5,
+          y: 0
+        },
+        {
+          x: 0,
+          y: avatarWidth-frameSize
+        },
+        {
+          x: avatarWidth-frameSize,
+          y: 0,
+        },
+        {
+          x: avatarWidth-frameSize,
+          y: avatarWidth-frameSize,
+        }
+      ];
+      framePosition.forEach((pos, index) => {
+        console.log(index);
+        console.log(pos);
+        const frameImg = canvas.createImage();
+        if (index == 0 && that.data.cornerFrameUrl[0].value) {
+          console.log(that.data.cornerFrameUrl[0].value)
+          frameImg.src = that.data.cornerFrameUrl[0].value;
+        }else if(index == 1 && that.data.cornerFrameUrl[1].value) {
+          console.log(that.data.cornerFrameUrl[1].value)
+          frameImg.src = that.data.cornerFrameUrl[1].value;
+        }else if(index == 2 && that.data.cornerFrameUrl[2].value) {
+          console.log(that.data.cornerFrameUrl[2].value)
+          frameImg.src = that.data.cornerFrameUrl[2].value;
+        }else if(index == 3 && that.data.cornerFrameUrl[3].value){
+          console.log(that.data.cornerFrameUrl[3].value)
+          frameImg.src = that.data.cornerFrameUrl[3].value;
+        }
+        frameImg.onload = () => {
+          ctx.drawImage(frameImg, pos.x, pos.y, frameSize, frameSize);
+        }
+      });
+    }
+
   },
-  loadAvatar(){
+  loadAvatar() {
     //检查本地头像加载
     const avatarUrl = wx.getStorageSync('avatarUrl');
-    if(avatarUrl){
+    if (avatarUrl) {
       this.setData({
         avatarUrl,
-        avatarWrapperShow:false,
+        avatarWrapperShow: false,
       });
-    }else{
-      avatarUrl=defaultAvatarUrl
+    } else {
+      avatarUrl = defaultAvatarUrl
     }
   },
-  saveAvatarPic:function(){
+  saveAvatarPic: function () {
     var that = this;
     this.triggerEvent('saveAvatarPic');
     // 把canvas转化成临时文件url
     wx.canvasToTempFilePath({
       // canvasId:'avatarCanvas',
-      canvas:that.data.canvas,
-      x:0,
-      y:0,
-      width:512,
-      height:512,
-      destHeight:100,
-      destWidth:100,
-      success:(res)=>{
+      canvas: that.data.canvas,
+      x: 0,
+      y: 0,
+      width: 512,
+      height: 512,
+      destHeight: 100,
+      destWidth: 100,
+      success: (res) => {
         const tempFilePath = res.tempFilePath;
         console.log(tempFilePath);
         //请求授权保存相册
         wx.authorize({
           scope: 'scope.writePhotosAlbum',
-          success:(res)=>{
+          success: (res) => {
             wx.saveImageToPhotosAlbum({
               filePath: tempFilePath,
-              success:(saveRes)=>{
+              success: (saveRes) => {
                 console.log('save success')
                 wx.showToast({
                   title: '保存成功！',
-                  icon:'success',
-                  duration:1000
+                  icon: 'success',
+                  duration: 1000
                 });
               },
-              fail:(err)=>{
-                console.log('save failed',err)
+              fail: (err) => {
+                console.log('save failed', err)
                 wx.showToast({
                   title: '保存失败',
-                  icon:'none',
-                  duration:1000
+                  icon: 'none',
+                  duration: 1000
                 });
               }
             });
           },
-          fail:(resErr)=>{
-            console.log('auth failed',resErr);
+          fail: (resErr) => {
+            console.log('auth failed', resErr);
             wx.showModal({
-              title:'授权提示',
-              content:'需要获取保存图片权限',
-              showCancel:false,
-              confirmText:'设置',
-              success:(modalRes)=>{
-                if(modalRes.confirm){
+              title: '授权提示',
+              content: '需要获取保存图片权限',
+              showCancel: false,
+              confirmText: '设置',
+              success: (modalRes) => {
+                if (modalRes.confirm) {
                   wx.openSetting({
-                    success:(setRes)=>{
+                    success: (setRes) => {
                       console.log('set')
                     }
                   });
@@ -182,22 +257,30 @@ Page({
           }
         });
       },
-      fail:(err)=>{
-        console.log('canvas to file failed',err);
+      fail: (err) => {
+        console.log('canvas to file failed', err);
         wx.showToast({
           title: '头像转换失败',
-          icon:'none',
-          duration:1000
+          icon: 'none',
+          duration: 1000
         });
       }
-    },this);
+    }, this);
+  },
+  switchDiyTab:function(event){
+    this.triggerEvent('switchDiyTab')
+    let diy = event.currentTarget.dataset.diy;
+    this.setData({
+      diyType : diy
+    })
+    console.log(this.data.diyType)
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
     this.loadAvatar(); //检查本地头像加载
-    this.drawAvatarFrame();//绘制头像和头像框
+    this.drawAvatarFrame(); //绘制头像和头像框
   },
 
   /**
@@ -205,7 +288,7 @@ Page({
    */
   onShow() {
     this.loadAvatar(); //检查本地头像加载
-    this.drawAvatarFrame();//绘制头像和头像框
+    this.drawAvatarFrame(); //绘制头像和头像框
   },
 
   /**
@@ -227,7 +310,7 @@ Page({
    */
   onPullDownRefresh() {
     this.loadAvatar(); //检查本地头像加载
-    this.drawAvatarFrame();//绘制头像和头像框
+    this.drawAvatarFrame(); //绘制头像和头像框
   },
 
   /**
@@ -243,16 +326,77 @@ Page({
   onShareAppMessage() {
 
   },
-  goBack:function(){
+  goBack: function () {
     this.triggerEvent('goBack');
     wx.navigateBack({
-      delta:1,
-      success:function(res){
+      delta: 1,
+      success: function (res) {
         console.log('back success');
       },
-      fail:function(err){
-        console.log('back failed',err);
+      fail: function (err) {
+        console.log('back failed', err);
       }
     });
-  }
+  },
+  switchOuterFrame:function(event){
+    this.triggerEvent('switchOuterFrame')
+    let id = event.currentTarget.dataset.id;
+    if(id==0){
+      this.setData({
+        outerFrameUrl:'',
+      })
+      console.log('outer frame set none')
+    }else{
+      this.setData({
+        outerFrameUrl:this.data.outerFrames[id].frameSrc,
+      })
+      console.log(this.data.outerFrameUrl)
+    }
+    this.drawAvatarFrame()
+  },
+  switchInnerFrame:function(event){
+    this.triggerEvent('switchInnerFrame')
+    let id = event.currentTarget.dataset.id-10;
+    if(id==0){
+      this.setData({
+        innerFrameUrl:'',
+      })
+      console.log('inner frame set none')
+    }else{
+      this.setData({
+        innerFrameUrl:this.data.innerFrames[id].frameSrc,
+      })
+      console.log(this.data.innerFrameUrl)
+    }
+    this.drawAvatarFrame()
+  },
+  switchCornerFrame:function(event){
+    this.triggerEvent('switchCornerFrame')
+    let id = event.currentTarget.dataset.id;
+    if(id==100){
+      this.setData({
+        cornerFrameUrl:['','','','']
+      })
+      console.log('corner frame set none')
+    }else if(id<110){
+      this.setData({
+        'cornerFrameUrl[0].value':this.data.cornerFrames[id-100].frameSrc,
+      })
+    }else if(id<120){
+      this.setData({
+        'cornerFrameUrl[1].value':this.data.cornerFrames[id-110].frameSrc,
+      })
+    }else if(id<130){
+      this.setData({
+        'cornerFrameUrl[2].value':this.data.cornerFrames[id-120].frameSrc,
+      })
+    }else if(id<140){
+      this.setData({
+        'cornerFrameUrl[3].value':this.data.cornerFrames[id-130].frameSrc,
+      })
+    }
+    console.log(this.data.cornerFrameUrl)
+    this.drawAvatarFrame()
+  },
+
 })
