73b7ed9365f16aa8ea4006f3dae78dc542823014
lori-pipi <zjch123@163.com>
2024-12-07 22:10:28 +0800

    commit to establish some collection views

--- pages/plantCollection/plantCollection.js
+++ pages/plantCollection/plantCollection.js
@@ -1,16 +1,5 @@
 Page({
-    takePhoto() {
-      const ctx = wx.createCameraContext()
-      ctx.takePhoto({
-        quality: 'high',
-        success: (res) => {
-          this.setData({
-            src: res.tempImagePath
-          })
-        }
-      })
-    },
-    error(e) {
-      console.log(e.detail)
-    }
-  })
+
+
+    
+})
