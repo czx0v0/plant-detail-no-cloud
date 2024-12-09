7dd4d6a9c09de62f1f066e2db7323349a99b7595
lori-pipi <zjch123@163.com>
2024-12-09 15:03:09 +0800

    update homepage and fixbug on collectionPage

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,18 @@ Page({
         url: '../map/map' // 个人信息页路径
       });
     },
+
+     goToMap() {
+      wx.navigateTo({
+        url: '../map/map' // 个人信息页路径
+      });
+    },
     
+    navigateToSearchPage() {
+        wx.navigateTo({
+            url: '/pages/plantSearch/plantSearch',
+          });
+    },
     // 跳转到植物识别页
     goToRecognition() {
       wx.navigateTo({
